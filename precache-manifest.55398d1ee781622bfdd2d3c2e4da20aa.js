4f9591e3cddb0b3cfc7d55ec85b45ab399084dfc
George Markou <gmarkou@gmail.com>
2020-02-13 14:24:43 +0200

    first commit

---
+++ precache-manifest.55398d1ee781622bfdd2d3c2e4da20aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df3333f98b14df86e5f42aa18103103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1abce9c7be8e54d4cc7e",
+    "url": "/static/css/main.4efe5dc0.chunk.css"
+  },
+  {
+    "revision": "a7fe7984aba811ea9449",
+    "url": "/static/js/2.0e667aa1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0e667aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abce9c7be8e54d4cc7e",
+    "url": "/static/js/main.354c50bd.chunk.js"
+  },
+  {
+    "revision": "8964542be1cc122cf6f3",
+    "url": "/static/js/runtime-main.233bea61.js"
+  }
+]);
